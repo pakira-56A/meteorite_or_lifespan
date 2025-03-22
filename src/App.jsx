8b340342c3c274688233e24726c8711fa5c3e4eb
pakira-56A <pakira.rrr@gmail.com>
2025-03-22 21:13:07 +0900

    fix: ヘッダーをコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 import "./App.css"
 import RouletteWheel from "./components/RouletteWheel"
 import WeatherFortune from "./components/WeatherFortune"
+import Header from "./components/Header"
+
 
 function App() {
   const [isSpinning, setIsSpinning] = useState(false)    // 初期値はルーレットが回転していない
@@ -297,15 +299,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <h2 style={{ color: "blue", marginTop: "0px", backgroundColor: "#FFFFAA", borderRadius: "10px" }}>
-        お天気ルーレットおみくじ
-      </h2>
-
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-        <img src="/images/お天気お姉さん.png" alt="お天気お姉さん" style={{ width: "120px" }} />
-        <img src="/images/おみくじ.png" alt="おみくじ" style={{ width: "90px", marginLeft: "30px", opacity: "0.8" }} />
-      </div>
-      <div style={{ color: "#0088ff", margin: "15px 0px" }}>気象庁さんの予報データでおみくじしよう！</div>
+      <Header />
 
       <main>
         <div className="roulette-container">
